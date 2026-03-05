1bcfeda7f346ba81d85bf94216f5a717fc42e642
Priyanshu Gupta <realpriyanshugupta@gmail.com>
2026-03-05 09:27:11 +0530

    nextra docs init

---
+++ apps/docs/app/layout.tsx
@@ -0,0 +1,51 @@
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
+
+export const metadata = {
+  title: "OpenSlop Docs",
+  description: "OpenSlop Documentation",
+};
+
+const banner = <Banner storageKey="some-key">OpenSlop 1.0 is released 🎉</Banner>;
+const navbar = (
+  <Navbar
+    logo={<b>OpenSlop</b>}
+    logoLink="/"
+  />
+);
+const footer = <Footer>MIT {new Date().getFullYear()} © OpenSlop.</Footer>;
+
+export default async function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body>
+        <Layout
+          banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/priyanshuguptadev/openslop/tree/main/apps/docs"
+          footer={footer}
+        // ... Your additional layout options
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  );
+}
